f69f37171f108360b8f781fb768fa75c314508f3
Nicolas Buquet <>
2021-01-19 15:37:19 +0100

    [ITEMLIST] : item List Controller

--- LBC Sample/Controllers/ItemsViewController.swift
+++ LBC Sample/Controllers/ItemsViewController.swift
@@ -6,3 +6,92 @@
 //
 
 import Foundation
+import UIKit
+
+class ItemsViewController: AppBaseViewController {
+    private static let CELL_REUSE_ID = #file + "_REUSE_ID"
+    
+    // Use collectionView to be able to easily display Cards on iPad (larger screen)
+    lazy var collectionView: UICollectionView = { [unowned self] in
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .background
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        collectionView.register(ItemCell.self, forCellWithReuseIdentifier: Self.CELL_REUSE_ID)
+        return collectionView
+    }()
+    
+    var data = [Item]()
+    
+    override init() {
+        super.init()
+        self.reloadData()
+        
+        self.navigationItem.titleView = UIImageView(image: UIImage(named: "leBonCoinNavBarTitle"))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func reloadData() {
+        AppData.shared.fetchItems { (items, errorr) in
+            guard let items = items
+            else {
+                // TODO: Display alert
+                return
+            }
+            self.data.removeAll()
+            self.data.append(contentsOf: items)
+            self.collectionView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.addSubview(self.collectionView)
+        
+        self.layoutContent()
+    }
+    
+    func layoutContent() {
+        self.view.translatesAutoresizingMaskIntoConstraints = true
+        
+        let constraints = [
+            self.collectionView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1.0),
+            self.collectionView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1.0),
+            self.collectionView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.collectionView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+    }
+}
+
+extension ItemsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Self.CELL_REUSE_ID, for: indexPath) as! ItemCell
+        // Populate cell
+        cell.item = self.data[indexPath.row]
+        return cell
+    }
+    
+    
+    
+    
+}
+
+extension ItemsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.size.width, height: 64.0)
+    }
+}
--- LBC Sample/Views/ItemCell.swift
+++ LBC Sample/Views/ItemCell.swift
@@ -6,3 +6,103 @@
 //
 
 import Foundation
+import UIKit
+
+class ItemCell: UICollectionViewCell {
+    private static let noImage = UIImage(named: "noImage")
+    
+    var item: Item? {
+        didSet {
+            self.updateContent()
+        }
+    }
+    
+    private var photoFetchRequest: NBNetwork.Request?
+    
+    let photo: UIImageView = {
+        let imageView = UIImageView(image: ItemCell.noImage)
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let imageLoader: UIActivityIndicatorView = {
+        let aiView = UIActivityIndicatorView(style: .gray)
+        aiView.translatesAutoresizingMaskIntoConstraints = false
+        aiView.hidesWhenStopped = true
+        aiView.stopAnimating() // start stopped and hidden
+        return aiView
+    }()
+    
+    let title: UILabel = {
+        let lbl = UILabel(text: "Items List")
+        lbl.font = UI.itemCellTitleFont
+        lbl.numberOfLines = 2
+        lbl.textColor = .text
+        return lbl
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.backgroundColor = .background
+        self.addSubview(self.photo)
+        self.addSubview(self.imageLoader)
+        self.addSubview(self.title)
+        
+        self.layoutContent()
+    }
+    
+    private func layoutContent() {
+        let constraints = [
+            self.photo.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1.0),
+            self.photo.widthAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1.0),
+            self.photo.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.photo.topAnchor.constraint(equalTo: self.topAnchor),
+            self.imageLoader.centerXAnchor.constraint(equalTo: self.photo.centerXAnchor),
+            self.imageLoader.centerYAnchor.constraint(equalTo: self.photo.centerYAnchor),
+            self.title.leadingAnchor.constraint(equalTo: self.photo.trailingAnchor, constant: 16.0),
+            self.title.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16.0),
+            self.title.centerYAnchor.constraint(equalTo: self.photo.centerYAnchor)
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.title.text = nil
+        self.photo.image = Self.noImage
+    }
+    
+    private func updateContent() {
+        if self.photoFetchRequest != nil {
+            // Cancel any pending request
+            self.photoFetchRequest!.cancel()
+        }
+        self.photoFetchRequest = nil
+        self.imageLoader.stopAnimating()
+        
+        self.title.text = self.item?.title ?? "<no title>"
+        
+        if let thumbUrl = self.item?.image.thumb {
+        self.photoFetchRequest = AppNetwork.shared.fetchImage(imageUrlString: thumbUrl, { (data, error) in
+            if let data = data {
+                self.photo.image = UIImage(data: data)
+            }
+            else {
+                // load default missing image
+                self.photo.image = Self.noImage
+            }
+            
+            self.imageLoader.stopAnimating()
+            self.photoFetchRequest = nil
+        })
+            self.imageLoader.startAnimating()
+        }
+    }
+}
