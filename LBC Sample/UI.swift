a8dd309e415402e34996a36f980d5abef744cc64
Nicolas Buquet <>
2021-01-21 15:29:41 +0100

    [ItemDetail] : UI settings

--- LBC Sample/UI.swift
+++ LBC Sample/UI.swift
@@ -12,9 +12,14 @@ class UI {
     static let navigationBarTitle = UIFont.systemFont(ofSize: 21.0)
     static let itemCellTitleFont = UIFont.systemFont(ofSize: 14.0)
     static let itemCellPriceFont = UIFont.boldSystemFont(ofSize: 18.0)
-    static let itemCellBadgeSmallFont = UIFont.boldSystemFont(ofSize: 10.0)
-    static let itemCellBadgeBigFont = UIFont.boldSystemFont(ofSize: 18.0)
+    static let badgeSmallFont = UIFont.boldSystemFont(ofSize: 10.0)
+    static let badgeMediumFont = UIFont.boldSystemFont(ofSize: 14.0)
+    static let badgeBigFont = UIFont.boldSystemFont(ofSize: 18.0)
     static let loadingFont = UIFont.systemFont(ofSize: 14.0)
+    static let itemDetailTitleFont = UIFont.boldSystemFont(ofSize: 24.0)
+    static let itemDetailPriceFont = UIFont.boldSystemFont(ofSize: 24.0)
+    static let itemDetailDescriptionFont = UIFont.systemFont(ofSize: 16.0)
+    static let itemDetailInformationFont = UIFont.systemFont(ofSize: 12.0)
 }
 
 extension UIColor {
